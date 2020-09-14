e5074d2074b26424c43a000cb6e6ae177c658233
angusho1 <angusho.77@gmail.com>
2020-09-13 23:17:20 -0700

    fixed config and env issues

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const config = {
-    GEOCODING_KEY = process.env.GEOCODING_KEY,
-    MAPS_KEY = process.env.MAPS_KEY
+    "GEOCODING_KEY" : process.env.GEOCODING_KEY,
+    "MAPS_KEY" : process.env.MAPS_KEY
 };
