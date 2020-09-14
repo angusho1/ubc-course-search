479dc637aef3fb05367d6fb3a9f0857bec8e953e
angusho1 <angusho.77@gmail.com>
2020-09-13 23:38:27 -0700

    attempting config issue fix

--- config.js
+++
@@ -1,4 +0,0 @@
-const config = {
-    "GEOCODING_KEY" : process.env.GEOCODING_KEY,
-    "MAPS_KEY" : process.env.MAPS_KEY
-};
---
+++ setConfig.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const configContent = `const config = {
+    GEOCODING_KEY : ${process.env.GEOCODING_KEY},
+    MAPS_KEY : ${process.env.MAPS_KEY}
+}`;
+fs.writeFile('config.js', configContent, (err) => {
+    if (err) throw new Error;
+});
